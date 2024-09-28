08feb70a9adb4d7847975be946dc67a51ad6d9bd
Ruslan <35475@iitu.edu.kz>
2024-09-28 06:09:58 +0500

    Hello world

---
+++ src/test/java/com/uniproject/universityproject/UniversityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniproject.universityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
